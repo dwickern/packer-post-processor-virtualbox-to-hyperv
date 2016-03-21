812b8a358bdb7788489006fd6d1e2ab8e3dfedef
Derek Wickern <dwickern@gmail.com>
2016-03-21 11:19:16 -0700

    Initial commit

---
+++ hyperv/provider.go
@@ -0,0 +1,64 @@
+package hyperv
+
+// SOURCE: https://github.com/mitchellh/packer/pull/1659
+
+import (
+	"fmt"
+	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/post-processor/vagrant"
+	"path/filepath"
+	"os"
+)
+
+type HypervProvider struct{}
+
+func (p *HypervProvider) KeepInputArtifact() bool {
+	return false
+}
+
+func (p *HypervProvider) Process(ui packer.Ui, artifact packer.Artifact, dir string) (vagrantfile string, metadata map[string]interface{}, err error) {
+
+	// Create the metadata
+	metadata = map[string]interface{}{"provider": "hyperv"}
+
+	var outputDir string
+
+	// Copy all of the original contents into the temporary directory
+	for i, path := range artifact.Files() {
+
+		// Vargant requires specific dir structure for hyperv
+		// hyperv builder creates the structure in the output dir
+		// we have to keep the structure in a temp dir
+
+		// hyperv artifact put the output dir path as the first elem
+		if i == 0 {
+			outputDir = path
+			continue
+		}
+
+		ui.Message(fmt.Sprintf("Copying: %s", path))
+
+		var rel string
+
+		rel, err = filepath.Rel(outputDir, filepath.Dir(path))
+
+		if err != nil {
+			return
+		}
+
+		dstDir := filepath.Join(dir, rel)
+
+		if _, err = os.Stat(dstDir); err != nil {
+			if err = os.MkdirAll(dstDir, 0755); err != nil {
+				return
+			}
+		}
+
+		dstPath := filepath.Join(dstDir, filepath.Base(path))
+		if err = vagrant.CopyContents(dstPath, path); err != nil {
+			return
+		}
+	}
+
+	return
+}
---
+++ post-processor.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"packer-post-processor-virtualbox-to-hyperv/hyperv"
+
+	vbox "github.com/mitchellh/packer/builder/virtualbox/common"
+	"github.com/mitchellh/packer/helper/config"
+	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/post-processor/vagrant"
+	"github.com/mitchellh/packer/template/interpolate"
+)
+
+type Config struct {
+	vagrant.Config `mapstructure:",squash"`
+
+	StagingDir string `mapstructure:"staging_directory"`
+	VMName     string `mapstructure:"vm_name"`
+
+	ctx interpolate.Context
+}
+
+type PostProcessor struct {
+	config Config
+
+	// embed the "vagrant" post-processor to work around https://github.com/mitchellh/packer/issues/1653
+	vagrant vagrant.PostProcessor
+
+	// used to convert the hard drive format
+	virtualbox vbox.Driver
+}
+
+func (p *PostProcessor) Configure(raws ...interface{}) error {
+	err := config.Decode(&p.config, &config.DecodeOpts{
+		Interpolate:        true,
+		InterpolateContext: &p.config.ctx,
+		InterpolateFilter: &interpolate.RenderFilter{
+			Exclude: []string{},
+		},
+	}, raws...)
+	if err != nil {
+		return err
+	}
+
+	// configure defaults
+	if p.config.StagingDir == "" {
+		p.config.StagingDir = fmt.Sprintf("output-%s-hyperv", p.config.PackerBuildName)
+	}
+	if p.config.VMName == "" {
+		p.config.VMName = fmt.Sprintf("packer-%s-%d", p.config.PackerBuildName, interpolate.InitTime.Unix())
+	}
+
+	// configure "vagrant" post-processor
+	if err := p.vagrant.Configure(raws...); err != nil {
+		return fmt.Errorf("Failed to configure vagrant post-processor: %s", err)
+	}
+
+	// find the VBoxManage executable
+	if p.virtualbox, err = vbox.NewDriver(); err != nil {
+		return fmt.Errorf("Failed to create VirtualBox driver: %s", err)
+	}
+
+	return nil
+}
+
+func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, bool, error) {
+	if artifact.BuilderId() != "mitchellh.virtualbox" {
+		err := fmt.Errorf("Unknown artifact type: %s\nCan only import from virtualbox artifacts.", artifact.BuilderId())
+		return nil, false, err
+	}
+
+	outVirtualMachines := filepath.Join(p.config.StagingDir, "Virtual Machines")
+	if err := os.MkdirAll(outVirtualMachines, 0755); err != nil {
+		return nil, false, fmt.Errorf("Failed to create output directory: %s", err)
+	}
+
+	outVirtualHardDisks := filepath.Join(p.config.StagingDir, "Virtual Hard Disks")
+	if err := os.MkdirAll(outVirtualHardDisks, 0755); err != nil {
+		return nil, false, fmt.Errorf("Failed to create output directory: %s", err)
+	}
+
+	vm := filepath.Join(outVirtualMachines, "vm.xml")
+	if err := ioutil.WriteFile(vm, p.CreateVM(), 0644); err != nil {
+		return nil, false, fmt.Errorf("Failed to write vm.xml: %s", err)
+	}
+
+	vmdk, err := FindVirtualHardDisk(artifact)
+	if err != nil {
+		return nil, false, fmt.Errorf("Failed to find VirtualBox hard disk: %s", err)
+	}
+
+	// the hard disk filename must match the `pathname` in vm.xml
+	vhd := filepath.Join(outVirtualHardDisks, "disk.vhd")
+	if err = p.virtualbox.VBoxManage("clonehd", vmdk, vhd, "--format", "vhd"); err != nil {
+		return nil, false, fmt.Errorf("Failed to convert VMDK to VHD: %s", err)
+	}
+
+	newArtifact, err := hyperv.NewArtifact(p.config.StagingDir)
+	if err != nil {
+		return nil, false, fmt.Errorf("Failed to create Hyper-V artifact: %s", err)
+	}
+
+	return p.vagrant.PostProcessProvider("hyperv", new(hyperv.HypervProvider), ui, newArtifact)
+}
+
+func (p *PostProcessor) CreateVM() []byte {
+	vm := VM_XML
+	vm = strings.Replace(vm, "INSERT_VM_NAME_HERE", p.config.VMName, 1)
+	// TODO: update `creation_time`
+	return []byte(vm)
+}
+
+func FindVirtualHardDisk(artifact packer.Artifact) (string, error) {
+	for _, path := range artifact.Files() {
+		if strings.ToLower(filepath.Ext(path)) == ".vmdk" {
+			return path, nil
+		}
+	}
+
+	return "", errors.New("Input artifact did not have a VMDK file")
+}
